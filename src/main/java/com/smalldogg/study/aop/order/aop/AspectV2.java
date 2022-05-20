b5fbf72390ac5c69505053d954821733ca7827ba
small-dogg <sy920112@naver.com>
2022-05-20 19:31:29 +0900

    feat: 스프링 AOP 구현   - AOP 구현   - 포인트컷 분리   - 어드바이스 추가   - 포인트컷 참조   - 어드바이스 순서   - 어드바이스 종류

---
+++ src/main/java/com/smalldogg/study/aop/order/aop/AspectV2.java
@@ -0,0 +1,22 @@
+package com.smalldogg.study.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    //com.smalldogg.study.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* com.smalldogg.study.aop.order..*(..))")
+    private void allOrder() {} //pointcut signature
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
